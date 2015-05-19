300fbef4a60f369d1ed0d62be1832d7423d06a42
Felix Wetell <felix.wetell@elev.it-gymnasiet.se>
2015-05-19 15:14:57 +0200

    code not done

---
+++ lib/rovarizer.rb
@@ -0,0 +1,16 @@
+def rovarize(cleartext:)
+
+  consonant = ['B', 'C', 'D', 'F', 'G', 'H', 'J', 'K', 'L', 'M', 'N', 'P', 'Q', 'R', 'S', 'T', 'V', 'W', 'X', 'Z','b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'v', 'w', 'x', 'z']
+  crypt = ""
+  text = cleartext.scan /\w/
+
+  text.each do |char|
+    if consonant.include? char
+      crypt << char + "o" + char
+    else
+      crypt << char
+    end
+  end
+  return crypt
+
+end
