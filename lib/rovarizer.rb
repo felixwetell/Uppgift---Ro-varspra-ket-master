5e16dd5c5fd691d2b00fbda51eb9d5387b24002a
Felix Wetell <felix.wetell@elev.it-gymnasiet.se>
2015-05-22 15:21:14 +0200

    derovarize complete, working code

--- lib/rovarizer.rb
+++ lib/rovarizer.rb
@@ -18,3 +18,29 @@ def rovarize(cleartext:)
   return crypt
 
 end
+
+def derovarize(cipher:)
+
+  consonant = ['B', 'C', 'D', 'F', 'G', 'H', 'J', 'K', 'L', 'M', 'N', 'P', 'Q', 'R', 'S', 'T', 'V', 'W', 'X', 'Z','b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'v', 'w', 'x', 'z']
+  decrypt = ""
+  text = cipher.split(//)
+
+  text.each do |char|
+
+    if consonant.include? char
+      decrypt << char
+      text.delete_at(0)
+      text.delete_at(1)
+
+    else
+      decrypt << char
+
+    end
+  end
+  return decrypt
+
+end
+
+
+# text = "Hejsan 2lt Larsson!"
+# derovarize(cipher:rovarize(cleartext:text))
